c7f5c318b3efeee927a012ca6e0c6301e767ed31
Артём Бурмистров < >
2017-10-07 15:09:08 +0300

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using SimpleScanner;
+using SimpleParser;
+
+namespace SimpleCompiler
+{
+    public class SimpleCompilerMain
+    {
+        public static void Main()
+        {
+            string FileName = @"..\..\a.txt";
+            try
+            {
+                string Text = File.ReadAllText(FileName);
+
+                Scanner scanner = new Scanner();
+                scanner.SetSource(Text, 0);
+
+                Parser parser = new Parser(scanner);
+
+                var b = parser.Parse();
+                if (!b)
+                    Console.WriteLine("ЕГГОГ"); //Ошибка
+                else
+                {
+                    Console.WriteLine("Sytax tree has built"); //Синтаксическое дерево построено
+                    //foreach (var st in parser.root.StList)
+                    //Console.WriteLine(st);
+                }
+                //root.exec();
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Файл {0} не найден", FileName);
+            }
+            catch (LexException e)
+            {
+                Console.WriteLine("Lexical error. " + e.Message); //Лексическая ошибка
+            }
+            catch (SyntaxException e)
+            {
+                Console.WriteLine("Syntax error. " + e.Message); //Синтаксическая ошибка
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ ProgramTree.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+
+namespace ProgramTree
+{
+    public enum AssignType { Assign, AssignPlus, AssignMinus, AssignMult, AssignDivide };
+
+    public enum OpType { Plus, Minus };
+
+    public class Node // базовый класс для всех узлов    
+    {
+    }
+
+    public abstract class ExprNode : Node // базовый класс для всех выражений
+    {
+        public abstract int Eval();
+    }
+
+    public class IdNode : ExprNode
+    {
+        public string Name { get; set; }
+        public IdNode(string name) { Name = name; }
+        public int Value { get; set; }
+        public override int Eval()
+        {
+            return Value;
+        }
+    }
+
+    public class BinExprNode : ExprNode
+    {
+        public BinExprNode(ExprNode left, ExprNode right, OpType op)
+        {
+            Left = left;
+            Right = right;
+            Op = op;
+        }
+        public ExprNode Left { get; set; }
+        public ExprNode Right { get; set; }
+        public OpType Op { get; set; }
+        public override int Eval()
+        {
+            int res = Left.Eval();
+            switch (Op)
+            {
+                case OpType.Plus:
+                    res += Right.Eval();
+                    break;
+                case OpType.Minus:
+                    res -= Right.Eval();
+                    break;
+            }
+            return res;
+        }
+
+    }
+
+    public class IntNumNode : ExprNode
+    {
+        public int Num { get; set; }
+        public IntNumNode(int num) { Num = num; }
+        public override int Eval()
+        {
+            return Num;
+        }
+    }
+
+    public class StatementNode : Node // базовый класс для всех операторов
+    {
+    }
+
+    public class AssignNode : StatementNode
+    {
+        public IdNode Id { get; set; }
+        public ExprNode Expr { get; set; }
+        public AssignType AssOp { get; set; }
+        public AssignNode(IdNode id, ExprNode expr, AssignType assop = AssignType.Assign)
+        {
+            Id = id;
+            Expr = expr;
+            AssOp = assop;
+        }
+    }
+
+    public class CycleNode : StatementNode
+    {
+        public ExprNode Expr { get; set; }
+        public StatementNode Stat { get; set; }
+        public CycleNode(ExprNode expr, StatementNode stat)
+        {
+            Expr = expr;
+            Stat = stat;
+        }
+    }
+
+    public class BlockNode : StatementNode
+    {
+        public List<StatementNode> StList = new List<StatementNode>();
+        public BlockNode(StatementNode stat)
+        {
+            Add(stat);
+        }
+        public void Add(StatementNode stat)
+        {
+            StList.Add(stat);
+        }
+    }
+
+}
