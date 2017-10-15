65c863b0e3d1be7587cd7758f88ba23ffd618890
Alexander Pogorelov <deadgirlneversayno@gmail.com>
2017-10-15 19:53:53 +0300

    добавил класс Symbol, поправил таблицу символов

---
+++ Symbol.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleLang
+{
+    public class Symbol
+    {
+        public enum SymbolType { tint, tdouble };
+        
+        public SymbolType Type { get; set; }
+
+    }
+}
---
+++ SymbolTable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+namespace SimpleLang
+{
+    public class SymbolTable
+    {
+        private Hashtable table;
+        protected SymbolTable prev;
+
+        public SymbolTable(SymbolTable prev)
+        {
+            table = new Hashtable();
+            this.prev = prev;
+        }
+
+        public void Put(string id, Symbol sym)
+        {
+            table.Add(id, sym);
+        }
+
+        public Symbol Get(string id)
+        {
+            for (SymbolTable ct = this; ct != null; ct = ct.prev)
+            {
+                Symbol found = (Symbol)ct.table[id];
+                if (found != null)
+                    return found;
+            }
+            return null;
+        }
+    }
+}
